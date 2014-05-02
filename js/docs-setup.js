423edf2d10681320b41e3a6b7141adcf3a496f23
Jakub Hampl <honitom@seznam.cz>
2014-05-02 16:43:47 +0100

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,72 @@
+NG_DOCS={
+  "sections": {
+    "api": "angular-keyboard"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angular-keyboard",
+      "shortName": "angular-keyboard",
+      "type": "overview",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "angular-keyboard",
+      "keywords": "accessible add advanced allow allows alt angular-keyboard api app backspace basic capslock characters collections combinations command components concepts control created ctrl custom declare del delete designed directive docs dynamically easy enhance enter esc escape existing feature finally hand html individual infamous ins interactions intervals key keyboard keyboardhelp keyboardselectable keyboardshortcut keyboardshortcuts left linux listing manipulating meta mod modifier module names navigation option os overview pagedown pageup press presses pressing reading relevant represent represented represents require return separated separating sequences service shift short shortcut shortcuts showing simultaneously space spaces special straightforward strings syntax tab trigger user windows"
+    },
+    {
+      "section": "api",
+      "id": "angular-keyboard.directive:keyboardHelp",
+      "shortName": "keyboardHelp",
+      "type": "directive",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "Displays a formatted table of the currently active keyboard shortcuts. The directives that define these shortcuts need to be loaded in the current view.",
+      "keywords": "$scope active angular angular-keyboard api background-color border border-radius box-shadow ccc class color controller css ctrl current currently decrease define directive directives display displays document ea example f7f7f7 fff filter font-family font-size formatted function generates helvetica html increase inset issues js kbd keybinding keyboard keyboard-prevent-default keyboard-separator keyboard-shortcut keyboard-title line-height loaded margin module ng-click ng-controller padding purposes rgba sans-serif shift shortcuts solid style styling table text-align text-shadow tr true view x2325"
+    },
+    {
+      "section": "api",
+      "id": "angular-keyboard.directive:keyboardSelectable",
+      "shortName": "keyboardSelectable",
+      "type": "directive",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "Used for performing keyboard actions on lists of items. It must be specified on the parent element as ng-repeat and exposes another scope variable to it&#39;s elements: keyboardSelected which is a boolean indicating selection on the particular class.",
+      "keywords": "$index $scope action actions add alert angular angular-keyboard api attribute background boolean brackets children class click collection controller css ctrl custom decides default defaults directive el element elements entries establish event example exposes exposure finally function functionality hides html indicating item items js keybinding keyboard keyboard-prevent-default keyboard-selectable keyboard-selected keyboard-shortcut keyboard-title keyboardhelp keyboardpreventdefault keyboardselectable keyboardselected keyboardshortcut keyboardshortcuts kind link list lists matching module ng-class ng-click ng-controller ng-repeat number option parent performing prevents previous privateshortcut provide remove scope select select-next select-previous selected selectedclass selection selection-shortcut selectionaction selectnext selector selectprevious send service set shorcuts shortcut shortcuts space start string style thing trigger variable work yellow"
+    },
+    {
+      "section": "api",
+      "id": "angular-keyboard.directive:keyboardShortcut",
+      "shortName": "keyboardShortcut",
+      "type": "directive",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "Registers a keyboard shortcut for the default event for the attached element.",
+      "keywords": "$scope action actions active allows angular angular-keyboard api apply attach attached attaching behavior buttons call click conjuction controller ctrl customize decrease default defaults description details directive document dosomething ea element elements enter event example expose exposure expression forms function functions hides html increase js keyboard keyboard-action keyboard-prevent-default keyboard-shortcut keyboard-title keyboardaction keyboardhelp keyboardpreventdefault keyboardselectable keyboardshortcut keyboardshortcuts keyboardtitle keyboardtrigger links mod module ng-click ng-controller options prevents privateshortcut registers save saving selected selectionshortcut service set shift shortcut shortcutif shortcuts text title triggered triggering true user variaety work"
+    },
+    {
+      "section": "api",
+      "id": "angular-keyboard.filter:keybinding",
+      "shortName": "keybinding",
+      "type": "filter",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "Formats a keybinding string into a user accessible string.",
+      "keywords": "$sce accessible acii angular angular-keyboard api arrows background-color bind border border-radius box-shadow ccc character characters code color command css ctrl display example f7f7f7 fff filter flag font-family font-size formats helvetica html input inset js kbd keybinding left line-height margin module ng-bind-html output padding proper replaced represented representing rgba sans-serif sequences set solid special string style tags text-shadow true trusted usehtml user utf-8"
+    },
+    {
+      "section": "api",
+      "id": "angular-keyboard.service:KeyboardShortcuts",
+      "shortName": "KeyboardShortcuts",
+      "type": "object",
+      "moduleName": "angular-keyboard",
+      "shortDescription": "Centrally manages all active Keyboard shortcuts.",
+      "keywords": "$watch access action actions active alt angular angular-keyboard api array avaialable based behavior boolean called centrally click completes conditionally coresponding ctrl currently custom cycle default defining describes digest directive directives display event example exposed flag function hide href html included invoked js keybinding keyboard keyboardhelp keyboardshortcuts link manager manages method modifying module mouse my-dir mydir ng-click ng-repeat object objects observe options parameters prevent preventdefault private public register registered registers remove restrict return returns scope service set shortcut shortcuts string task taskmanager templateurl toggle true un-registers user val variables visisble wouldn"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api/angular-keyboard",
+  "scripts": [
+    "jquery.min.js",
+    "angular.js",
+    "angular-animate.js",
+    "angular-keyboard.js"
+  ]
+};
